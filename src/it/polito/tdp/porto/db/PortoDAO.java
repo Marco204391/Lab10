8fb12a3216d35d8d13637c6a7dc9ad46e760d564
Marco204391 <marco-frenc@hotmail.it>
2017-05-17 11:42:37 +0200

    1

--- src/it/polito/tdp/porto/db/PortoDAO.java
+++ src/it/polito/tdp/porto/db/PortoDAO.java
@@ -4,17 +4,24 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import it.polito.tdp.porto.model.Author;
+import it.polito.tdp.porto.model.AuthorIdMap;
 import it.polito.tdp.porto.model.Paper;
+import it.polito.tdp.porto.model.PaperIdMap;
 
 public class PortoDAO {
 
 	/*
 	 * Dato l'id ottengo l'autore.
 	 */
-	public Author getAutore(int id) {
+	public Author getAutore(int id, AuthorIdMap authorIdMap) {
 
+		List<Author> autori = new ArrayList<Author>();
 		final String sql = "SELECT * FROM author where id=?";
 
 		try {
@@ -27,6 +34,8 @@ public class PortoDAO {
 			if (rs.next()) {
 
 				Author autore = new Author(rs.getInt("id"), rs.getString("lastname"), rs.getString("firstname"));
+				autore= authorIdMap.put(autore);
+				autori.add(autore);			
 				return autore;
 			}
 
@@ -41,8 +50,9 @@ public class PortoDAO {
 	/*
 	 * Dato l'id ottengo l'articolo.
 	 */
-	public Paper getArticolo(int eprintid) {
+	public Paper getArticolo(int eprintid, PaperIdMap paperIdMap) {
 
+		List<Paper> papers = new ArrayList<Paper>();
 		final String sql = "SELECT * FROM paper where eprintid=?";
 
 		try {
@@ -55,6 +65,8 @@ public class PortoDAO {
 			if (rs.next()) {
 				Paper paper = new Paper(rs.getInt("eprintid"), rs.getString("title"), rs.getString("issn"),
 						rs.getString("publication"), rs.getString("type"), rs.getString("types"));
+				paper= paperIdMap.put(paper);
+				papers.add(paper);
 				return paper;
 			}
 
